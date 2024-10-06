6c6e24de64e2b4bf44810e5444d3e9dc493b4eea
heal333 <rohanaich333@gmail.com>
2024-10-07 00:40:44 +0530

    locking screen on suspend

--- .config/waybar/scripts/setWifiToggle.sh
+++ .config/waybar/scripts/setWifiToggle.sh
@@ -3,8 +3,6 @@ wifiState=$(nmcli r wifi)
 
 if [ $wifiState == "enabled" ];then
   nmcli radio wifi off
-  nmcli networking off
 elif [ $wifiState == "disabled" ];then
-  nmcli networking on
   nmcli radio wifi on
 fi
